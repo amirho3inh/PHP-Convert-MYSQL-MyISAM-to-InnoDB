c35a8823d9505db546d1b9e9cb0482445a6ea48b
amirho3in.hashemi <amirho3in.hashemi@gmail.com>
2018-09-10 16:24:44 +0430

    fix bug

--- convert-DB.php
+++ convert-DB.php
@@ -7,14 +7,17 @@ $database = 'database';
 
 $conn = mysqli_connect($server, $username, $password,$database) or die('unable to connect to msql server: ' . msql_error());
 
-$result = mysqli_query('show tables;');
+$result = mysqli_query($conn, "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_SCHEMA = '$database'
+        AND ENGINE = 'MyISAM'");
+
 if (!$result) {
     die('query failed: ');
 }
-while ($row = mysqli_fetch_array($rs)) {
+while ($row = mysqli_fetch_array($result)) {
     $tbl = $row[0];
     $sql = "ALTER TABLE $tbl ENGINE=INNODB;";
     #Command Reference: ALTER TABLE tableName ENGINE=MyISAM
-    mysqli_query($sql);
-    echo $tbl . ' : OK!';
+    mysqli_query($conn, $sql);
+    echo $tbl . ' : OK! </br>';
 }

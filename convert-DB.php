32d7f80277768087e0a5090e869b7156d6ec28f4
amirho3in.hashemi <amirho3in.hashemi@gmail.com>
2018-09-10 15:55:27 +0430

    edit functions

--- convert-DB.php
+++ convert-DB.php
@@ -5,17 +5,16 @@ $username = 'username';
 $password = 'password';
 $database = 'database';
 
-$conn = mysql_connect($server, $username, $password) or die('unable to connect to msql server: ' . msql_error());
-mysql_select_db($database, $conn) or die("unable to select database 'db': " . msql_error());
+$conn = mysqli_connect($server, $username, $password,$database) or die('unable to connect to msql server: ' . msql_error());
 
-$result = mysql_query('show tables;');
+$result = mysqli_query('show tables;');
 if (!$result) {
     die('query failed: ');
 }
-while ($row = mysql_fetch_array($rs)) {
+while ($row = mysqli_fetch_array($rs)) {
     $tbl = $row[0];
     $sql = "ALTER TABLE $tbl ENGINE=INNODB;";
     #Command Reference: ALTER TABLE tableName ENGINE=MyISAM
-    mysql_query($sql);
+    mysqli_query($sql);
     echo $tbl . ' : OK!';
 }
